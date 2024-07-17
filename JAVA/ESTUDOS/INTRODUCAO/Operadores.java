e1d282e3707cfa0c4f5b14d0a51fc90994e8a0e5
Laysa Bernardes <laysabernardes.ads@gmail.com>
2024-07-16 22:36:52 -0300

    Operadores - 15/19

---
+++ JAVA/ESTUDOS/INTRODUCAO/Operadores.java
@@ -0,0 +1,24 @@
+public class Operadores {
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 20;
+        
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1/num2);
+        System.out.println(num1*num2);
+
+        System.out.println(num1+num2 +"fica diferente dps de uma string"+ num1+num2);
+
+        // Operador de resto de divisão = %
+        int resto = 20%2;
+        System.out.println(resto);
+
+        //OPERADORES RELACIONAIS: <> , <= , >= , == , ! = 
+       
+       // Operadores logicos -> && (and) || (or) !  ( not) 
+       //
+
+
+    }
+}
